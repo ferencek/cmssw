8c3091f39abe14f26426e7cd38a251a366418f3c
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2020-05-22 16:33:39 +0200

    force running siPixelClustersPreSplitting@cpu

--- Validation/SiPixelHeterogeneousV/python/pixelValidateCluster_cfi.py
+++ Validation/SiPixelHeterogeneousV/python/pixelValidateCluster_cfi.py
@@ -4,5 +4,5 @@ from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 
 sipixelclustervalid = DQMEDAnalyzer("SiPixelValidateCluster",
     mightGet = cms.optional.untracked.vstring,
-    src = cms.InputTag("siPixelClustersPreSplitting")
+    src = cms.InputTag("siPixelClustersPreSplitting@cpu")
 )
